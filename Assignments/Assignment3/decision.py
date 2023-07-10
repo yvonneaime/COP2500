438c092aac265f65f6e7e282f480295b26ffc14d
Yvonne Aime <yvonneaime0@gmail.com>
2023-07-10 13:59:47 -0400

    up

--- Assignments/Assignment3/decision.py
+++ Assignments/Assignment3/decision.py
@@ -7,7 +7,21 @@ allowance = float(input("How much money do we have to spend?\n"))
 # Where are you eating? Options listed below
 options = int(input("Where would you like to go?\n"))
 
-# 1. Hot Dog Heaven 
-# 2. Hawkers
+
+# You chose option 1 (Hot Dog Heaven)
+# They sell the following food items:
+    # Chicago Hot Dog - $4.50
+    # Fries - $4.75
+# What would you you like to buy?
+
+# You chose option 2 (Hawkers)
+# They sell the following food items:
+    # Yi-Yi's Chicken Dumplings - $10.00 
+    # Roti Canai - $9.00
+# What would you you like to buy?
+
+    
 # 3. Black Bean Deli
+    #
+    #
 
