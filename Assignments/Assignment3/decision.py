4c0879196fc4388a48a429243f7f5d0934ab89d8
Yvonne Aime <yvonneaime0@gmail.com>
2023-07-12 07:01:30 -0400

    updates

--- Assignments/Assignment3/decision.py
+++ Assignments/Assignment3/decision.py
@@ -2,29 +2,73 @@
 # Yvonne Aime, COP2500, July 5 2023
 
 # First Statement 
-allowance = float(input("How much money do we have to spend?\n"))
+budget = float(input("How much money do we have to spend?\n"))
+options = int(input("Where would you like to go?\n 1. Hot Dog Heaven\n 2. Hawkers\n 3. Black Bean Deli\n"))
+print(options)
 
-# Where are you eating? Options listed below
-options = int(input("Where would you like to go?\n"))
+# set total to a value.. budget + 1 = set greater to ensure the total matches the value
+# WHILE budget is greater than total
+# Where are you eating? Options listed below (NUMBER BETWEEN 1 - 3)
+# IF NUM == 1 print() out 2 food items,
+# ask which food you want 
+# if it is food item1
+    #set price equal to a number 
+
+# How many would you like to buy
+# if that price is less than or equal to your budget
+    #you have found a place to eat 
+#otherwise
+    #you need to try again
+
+# Option 1 - Hot Dog Heaven 
+option1 = ("You chose option 1 (Hot Dog Heaven)\n They sell the following food items:\n")
+opt1_items = ("\t1. Chicago Hot Dog - $4.50\n \t2. Fries - $2.75\n")
+prompt1 = int(input(option1 + opt1_items + "What would you you like to buy?\n"))
+
+# Option 2 - Hawkers 
+option2 = ("You chose option 2 (Hawkers)\n They sell the following food items:\n\n")
+opt2_items = ("\t1. Yi-Yi's Chicken Dumplings - $10.00 \n \t2.Roti Canai - $9.00\n\n")
+prompt2 = option2 + opt2_items + "What would you you like to buy?\n"
+
+# Option 3 - Black Bean Deli
+option3 = ("You chose option 3 (Black Bean Deli)\n They sell the following food items:\n")
+opt3_items = ("\t1. Dark Chicken Platter - $14.60 \n \t2.Yuca Fries - $7.85 ")
+prompt3 = option3 + opt3_items + "What would you you like to buy?\n"
+
+# How many of each food item you want?
+food_count = int(input("How many would you like to buy?\n"))
+total = budget + 1
+if(options == 1):
+    print(prompt1)
+elif(prompt1 == 1 and prompt1 == 2):
+    print("How many would you like to buy?\n")
+if(prompt1 == 1):
+   summary = 4.50 * food_count
+   summary = f"That would cost ${summary}"
+   print(summary)
+elif(prompt1 == 2):
+    summary =  2.75 * food_count
+    print(summary)
 
 
-# You chose option 1 (Hot Dog Heaven)
-# They sell the following food items:
-    # Chicago Hot Dog - $4.50
-    # Fries - $4.75
-# What would you you like to buy?
-
-# You chose option 2 (Hawkers)
-# They sell the following food items:
-    # Yi-Yi's Chicken Dumplings - $10.00 
-    # Roti Canai - $9.00
-# What would you you like to buy?
 
 
-# You chose option 3 (Black Bean Deli)
-# They sell the following food items:
-    # Dark Chicken Platter - $14.60 
-    # Yuca Fries - $7.85
-# What would you you like to buy?
+# elif(options == 2):
+    # print(prompt2)
+# elif(options == 3):
+   #  print(prompt3)
+#while(budget > total )
+
+# Food items 
+
+
+
+
+
+
+
+
+
+
 
 
--- Notes/highlow.py
+++ Notes/highlow.py
@@ -3,6 +3,27 @@
 
 import random
 
-# chooses a number 1 - 100 listed as (1,100)
-hidden_num = random.randint(1,100)
-guess = -1
+
+
+
+
+# anything that is not in the range of 1 - 100 (could be 101,-4...)  
+# (setting it to equal to false for the while loop) 
+# number is outside the loop'
+play_game = True
+while(play_game == True):
+    # chooses a number 1 - 100 listed as (1,100)
+    hidden_num = random.randint(1,100)
+    guess = -1
+
+while(hidden_num != guess):
+    guess = int(input("What is your guess\n"))
+
+    if(guess == hidden_num):
+        print("You got it!!")
+    elif(guess > hidden_num):
+        print("Guess lower")
+    else:
+        print("Guess higher")
+again = input("Would you like to play again?\n")
+if(again == "Yes" or again == "yes"):
