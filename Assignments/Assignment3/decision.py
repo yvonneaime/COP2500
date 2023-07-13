746444031abbbd5a256f2b72e7c54aff4a544bfa
Yvonne Aime <yvonneaime0@gmail.com>
2023-07-12 20:57:17 -0400

    full send

--- Assignments/Assignment3/decision.py
+++ Assignments/Assignment3/decision.py
@@ -1,78 +1,57 @@
+# Yvonne Aime
+# COP2500
+# July 5 2023
+
 # Assignment 3 - Food Decisions
-# Yvonne Aime, COP2500, July 5 2023
 
-# First Statement 
 budget = float(input("How much money do we have to spend?\n"))
-options = int(input("Where would you like to go?\n 1. Hot Dog Heaven\n 2. Hawkers\n 3. Black Bean Deli\n"))
-print(options)
+options = int(input("Where would you like to go?\n" 
+                    "1. Hot Dog Heaven\n" "2. Hawkers\n" "3. Black Bean Deli\n"))
 
-# set total to a value.. budget + 1 = set greater to ensure the total matches the value
-# WHILE budget is greater than total
-# Where are you eating? Options listed below (NUMBER BETWEEN 1 - 3)
-# IF NUM == 1 print() out 2 food items,
-# ask which food you want 
-# if it is food item1
-    #set price equal to a number 
+total = budget + 1
 
-# How many would you like to buy
-# if that price is less than or equal to your budget
-    #you have found a place to eat 
-#otherwise
-    #you need to try again
+while(budget < total):
+    print(options)
 
-# Option 1 - Hot Dog Heaven 
-option1 = ("You chose option 1 (Hot Dog Heaven) \nThey sell the following food items:\n")
-opt1_items = ("\t1. Chicago Hot Dog - $4.50\n \t2. Fries - $2.75\n")
-prompt1 = int(input(option1 + opt1_items + "What would you you like to buy?\n"))
+    # Option 1 - Hot Dog Heaven
+    if (options == 1):
+        print("You chose option 1 (Hot Dog Heaven) \nThey sell the following food items:")       
+        print("\t1. Chicago Hot Dog - $4.50\n \t2. Fries - $2.75\n")
+        what_choice = int(input("What would you you like to buy?\n"))
+        food_count = int(input("How many would you like to buy?\n"))
+        if (what_choice == 1):
+            total = 4.50 * food_count
+        if (what_choice == 2):
+            total = 2.75 * food_count
+        print("\nThat would cost $%.2f"%total)
 
-# Option 2 - Hawkers 
-option2 = ("You chose option 2 (Hawkers)\n They sell the following food items:\n\n")
-opt2_items = ("\t1. Yi-Yi's Chicken Dumplings - $10.00 \n \t2.Roti Canai - $9.00\n\n")
-prompt2 = option2 + opt2_items + "What would you you like to buy?\n"
-
-# Option 3 - Black Bean Deli
-option3 = ("You chose option 3 (Black Bean Deli)\n They sell the following food items:\n")
-opt3_items = ("\t1. Dark Chicken Platter - $14.60 \n \t2.Yuca Fries - $7.85 ")
-prompt3 = option3 + opt3_items + "What would you you like to buy?\n"
-
-# How many of each food item you want?
-food_count = int(input("How many would you like to buy?\n"))
-loop_check = False  
-
-while(loop_check == False):
-
-    if(options == 1):
-        print(prompt1)
-    elif(prompt1 == 1 and prompt1 == 2):
-        print(food_count)
-    if(prompt1 == 1):
-        summary = 4.50 * food_count
-        summary = f"That would cost ${summary}"
-        print(summary)
-    elif(prompt1 == 2):
-        summary =  2.75 * food_count
-        print(summary)
+    # Option 2 - Hawkers
+    if (options == 2):
+        print("You chose option 2 (Hawkers) \nThey sell the following food items:")       
+        print("\t1. Chicken Dumplings - $10.00\n \t2. Roti Canai - $9.00\n")
+        what_choice = int(input("What would you you like to buy?\n"))
+        food_count = int(input("How many would you like to buy?\n"))
+        if (what_choice == 1):
+            total = 10.00 * food_count
+        if (what_choice == 2):
+            total = 9.00 * food_count
+        print("\nThat would cost $%.2f"%total)
+    
+    # Option 3 - Black Bean Deli
+    if (options == 3):
+        print("You chose option 3 (Black Bean Deli) \nThey sell the following food items:")       
+        print("\t1. Dark Chicken Platter - $14.60\n \t2. Yuca Fries - $7.85\n")
+        what_choice = int(input("What would you you like to buy?\n"))
+        food_count = int(input("How many would you like to buy?\n"))
+        if (what_choice == 1):
+            total = 14.60 * food_count
+        if (what_choice == 2):
+            total = 7.85 * food_count
+        print("\nThat would cost $%.2f"%total)
+    #
+    if(budget > total):
+        print("You have enough!\n✩ Enjoy! ✩ ")
+    else:
+        print("You dont have enough.")
         
-
-
-
-
-    # elif(options == 2):
-        # print(prompt2)
-    # elif(options == 3):
-    #  print(prompt3)
-    #while(budget > total )
-
-    # Food items 
-
-
-
-
-
-
-
-
-
-
-
 
