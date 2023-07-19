f09e6afb90c7069158727787a60606a2c9bdbec9
Yvonne Aime <yvonneaime0@gmail.com>
2023-07-18 23:21:28 -0400

    set up

--- Assignments/Assignment5/icons.py
+++ Assignments/Assignment5/icons.py
@@ -2,12 +2,29 @@
 # COP2500
 # July 18 2023
 
-#Here is what he needs:
+# Assignment 5: Restaurant Icons
 
-# A function for each of the icons
-# Each function will take the position it is located on the screen in addition to the size.  The position will be two different variables.  (X, Y, Size)
+# Each function will take the position it is located on the screen in addition to the size.  
+# The position will be two different variables.  (X, Y, Size)
 # The size of the icon will change based on the size variable
 # The X and Y is the starting position of the turtle
 # It should be oriented the same way each time it draws (HINT: setheading)
 # Uses color
-# Uses a loop within its drawing.  (HINT: A loop that always and only runs once is not a loop.)
+# Uses a loop within its drawing.  
+
+# Design 1 Function
+def design_1(x,y,size):
+    x = 0
+    y = 0
+    size = 0
+    return
+
+# Design 2 Function
+def design_2(x,y,size):
+    pass
+
+# Main Function
+def main():
+    design01 = design_1()
+    design02 = design_2()
+main()
