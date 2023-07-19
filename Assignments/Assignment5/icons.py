1f9c0f14e032141b82ce23a4a622522c0a54aad0
Yvonne Aime <yvonneaime0@gmail.com>
2023-07-18 22:59:09 -0400

    intro

---
+++ Assignments/Assignment5/icons.py
@@ -0,0 +1,13 @@
+# Yvonne Aime
+# COP2500
+# July 18 2023
+
+#Here is what he needs:
+
+# A function for each of the icons
+# Each function will take the position it is located on the screen in addition to the size.  The position will be two different variables.  (X, Y, Size)
+# The size of the icon will change based on the size variable
+# The X and Y is the starting position of the turtle
+# It should be oriented the same way each time it draws (HINT: setheading)
+# Uses color
+# Uses a loop within its drawing.  (HINT: A loop that always and only runs once is not a loop.)
