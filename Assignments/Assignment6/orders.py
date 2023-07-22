b52b3706d875bae68bb34542312b9f8b9caa5a7b
Yvonne Aime <yvonneaime0@gmail.com>
2023-07-21 22:10:28 -0400

    intro

--- Assignments/Assignment6/orders.py
+++ Assignments/Assignment6/orders.py
@@ -3,3 +3,11 @@
 # July 21 2023
 
 # Assignment 6: Taking Orders
+
+
+# Asks user 'how many items they would like to order' and is stored in 'orders' variable
+orders = input("How many items would you like to order?")
+# Main function - 
+def main():
+    pass
+main()
