771ced01925c30c428e07b981f441082396a82a2
Yvonne Aime <yvonneaime0@gmail.com>
2023-07-25 07:40:36 -0400

    final poosh

--- Assignments/Assignment6/orders.py
+++ Assignments/Assignment6/orders.py
@@ -1,51 +1,46 @@
 # Yvonne Aime
 # COP2500C
 # July 21 2023
-
 # Assignment 6: Taking Orders
 
-# Main function - 
+# menu_shortener() - Function
+def menu_shortener(meal):
+    # Formats words to all upper case 
+    meal = meal.upper()
+    # Split the item into words(based on spacing)
+    wordcheck = meal.split()
+    # Checks if it is 2-3 words 
+    if(len(wordcheck)== 2 or len(wordcheck)== 3):        
+        # Empty list, to append new words/abbreviations
+        wordcheck02=[]
+        for i in range(len(wordcheck)):
+            new = wordcheck[i]
+            #Appends new words 
+            wordcheck02.append(new[0])
+        # Rejoins words after split
+        combine = "".join(wordcheck02)  
+    # Checks if it is more 3 or more words 
+    elif(len(wordcheck)>3 or len(wordcheck) <2):
+        combine = meal[:3]
+    return combine
+
+# Main function  
 def main():
-  
     # Asks user 'how many items' and is stored in 'orders' variable
     orders = int(input("How many items would you like to order?\n"))
-
-        # Initializes 'items' variable within for loop 
+    # Initializes 'meal' variable within for loop 
     item_list = []
-
+    new_list = []
     for i in range(orders):
-            item = (input("What is item #"+ str(i+1)+ "?\n"))
-            item_list.append(item)
-
-
-    # Checks to see how long the list is 
-    length = len(item_list)
-
+            meal = (input("What is item #"+ str(i+1)+ "?\n"))
+            new_list.append(meal)
+            item_list.append(menu_shortener(meal))
+    # Header for final order printout
     print("Your Order:\n")
+    # Separates list with dash - and numbers the items ordered 
+    for i in range(len(item_list)):
+        print(str(i+1)+"."+item_list[i]+"-"+new_list[i])
+    # Prints out and returns final items
+    return new_list
 
-    # Separates lists with dash - 
-    for index in range(length):
-        print(index, "-" , item_list[index])
-    # Prints final items
-    return item_list
-
-
-# 'ordered_items' variable stores main () function   
-ordered_items = main()
-
-# menu_shortener() - Function
-def menu_shortener(item):
-    item = ""
-    # Split the item into words
-    words = item.split()
-    
-    if len(words) in (2, 3):
-        # If 2-3 words in input, take the first letter of each word and capitalize the code
-        words = words.capitalize()
-        print(words, "-", words)
-        #If there are more than three words or only one word
-    #get the first three letters of the first word
-    #Capitalize the code
-    # Example: "Bacon Lettuce Tomato Egg Turkey" becomes BAC
-    # Example: "Sandwich" becomes SAN
-    #if len(words==1 )
+main()
